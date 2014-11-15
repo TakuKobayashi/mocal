19b40a61adb8f053df17146d1d64e0240dd73b78
Genki Furumi <hyurumi@gmail.com>
2014-11-16 03:54:44 +0900

    Fix uri encode

--- app/controllers/api/top_controller.rb
+++ app/controllers/api/top_controller.rb
@@ -2,21 +2,21 @@
 class Api::TopController < Api::BaseController
 
   def asahi
-    @companies = Mst::Company.where("name like '%" + params[:q] + "%'")
+    @companies = Mst::Company.where("name like '%" + URI.decode(params[:q]) + "%'")
     if @companies.length >= 1
       @company = @companies[0]
     end
   end
 
   def companyDetail
-    @companies = Mst::Company.where("name like '%" + params[:q] + "%'")
+    @companies = Mst::Company.where("name like '%" + URI.decode(params[:q]) + "%'")
     if @companies.length >= 1
       @company = @companies[0]
     end
   end
 
   def social
-    @companies = Mst::Company.where("name like '%" + params[:q] + "%'")
+    @companies = Mst::Company.where("name like '%" + URI.decode(params[:q]) + "%'")
     if @companies.length >= 1
       @company = @companies[0]
     end
