dee4111a8d9c00307c7038d6189eae3ecf2679ef
Genki Furumi <hyurumi@gmail.com>
2014-11-16 09:46:07 +0900

    Change company_table into price_table

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,5 +54,5 @@ bench("mst_company") do
   book = ::Roo::Excelx.new("db/mst_data/mst_company.xlsx")
   import_sheet(book.sheet("articles"), Article)
   import_sheet(book.sheet("mst_companies"), Mst::Company)
-  import_sheet(book.sheet("companies"), Company)
+  import_sheet(book.sheet("prices"), Price)
 end
